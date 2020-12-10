d449b71507fe90791000ca02bd08afcf6f26343f
Youspeakcubic <75534476+Youspeakcubic@users.noreply.github.com>
2020-12-09 23:27:28 -0600

    Add files via upload

--- Todo.js
+++ Todo.js
@@ -1,19 +1,28 @@
-const currentDate  = new Date(),
+//This small snippet just gets the current date and displays it to the date element.
+var currentDate  = new Date(),
     currentDay   = currentDate.getDate()
     currentMonth = currentDate.getMonth()
 document.getElementById("date").innerHTML = `${currentDay}  /  ${currentMonth}  /   ${currentDate.getFullYear()}`;
-function textValidate() {
-   if (){
-      document.getElementById("item").placeholder = "You didnt write anything ";
-      addToList()
-  }
-  else {
-    addToList();
-  }
-}
-function addToList(){
+// addToList acquires the input and list container by id and adds the value of the input and add it to the current contents of the list container, when add to list button is clicked.
+function addToList() {
   var inp = document.getElementById("item").value;
   var list =document.getElementById("list").innerHTML;
-  document.getElementById("list").innerHTML = `${list} <h2 style ="font-family: 'Nerko One', cursive; font-size:45px;"> -${inp} </h2> `;
-  document.getElementById("item").value = "";
+  //if statement that does not allow the function to proceed if the input form is empty.
+  if (document.getElementById("item").value == '') {
+    document.getElementById("item").placeholder = 'Please enter something';
+  }
+  else {
+    document.getElementById("list").innerHTML = `${list} <h2 class="animate__fadeInRight" style ="font-family: 'Nerko One', cursive; font-size:45px;"> -${inp} </h2> `;
+    document.getElementById("item").value = "";
+    document.getElementById("item").placeholder = 'What do i need to do?';
+  }
+}
+//Save entries function creates a local storage and then take the html of the list container and saves it as the saveList key.
+function saveEntries() {
+  var finalList = document.getElementById("list");
+  localStorage.setItem('saveList' , finalList.innerHTML);
+}
+//Load list function take previously saved saveList key and inserts it to the list container.
+function loadList() {
+  document.getElementById("list").innerHTML = localStorage.getItem('saveList');
 }
